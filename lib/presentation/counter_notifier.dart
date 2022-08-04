00c2b0c9954d56e31f76e8a737929e2b7573b620
まさたか <shinonome419@icloud.com>
2022-08-04 14:54:42 +0900

    add: Added CounterNotifier with hooks_riverpod
    Added CounterNotifier with hooks_riverpod.

---
+++ lib/presentation/counter_notifier.dart
@@ -0,0 +1,16 @@
+import 'package:sample_app/domain/count.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final counterProvider = StateNotifierProvider<CounterNotifier, Count>(
+  (ref) => CounterNotifier(
+    const Count(0),
+  ),
+);
+
+class CounterNotifier extends StateNotifier<Count> {
+  CounterNotifier(Count state) : super(state);
+
+  void increment() {
+    state = state.copyWith(value: state.value + 1);
+  }
+}
